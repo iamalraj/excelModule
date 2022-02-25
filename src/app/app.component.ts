ae9e9a4eedccfe53a29735c4d47c407399212b96
iamalraj <ar4amal.raj@gmail.com>
2022-02-25 09:39:12 +0530

    Commiting changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: AOA = [[], []];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  /*Read from excel file on every file change */
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>evt.target;
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
   title = 'excelModule';
   /*File Name*/
   fileName = 'ExcelSheet.xlsx';
-  /*Dummy user values*/
+
+  /*Export to excel implementation*/
+  exportExcel(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
+}
+
+/*Dummy user values*/
+/*
   userList = [
     {
       id: 1,
@@ -46,18 +93,12 @@ export class AppComponent {
       username: 'Batman',
       email: 'batman@mail.com',
     },
+
+    {
+      id: 6,
+      name: 'Clarke Kent',
+      username: 'Superman',
+      email: 'superman@mail.com',
+    },
   ];
-
-  exportexcel(): void {
-    /* pass here the table id */
-    let element = document.getElementById('excel-table');
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
-
-    /* generate workbook and add the worksheet */
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    /* save to file */
-    XLSX.writeFile(wb, this.fileName);
-  }
-}
+  */
